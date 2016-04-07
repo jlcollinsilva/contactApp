bc3bcbcf5d6efd27f162c94af355a0856cdd4b31
jlcollinsilva <jlcollinsilva@gmail.com>
2016-04-06 20:50:06 -0500

    Delete contactFunctions.js

--- contactFunctions.js
+++
@@ -1,7 +0,0 @@
-function showContact(i) {
-var contactArr = JSON.parse(localStorage.getItem('contactList'));
-
-$('#contactName').val(contactArr[i].contactName);
-$('#contactPhone').val(contactArr[i].contactPhone);
-$('#contactEmail').val(contactArr[i].contactEmail);
-}
